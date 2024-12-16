99e5236c236f75217d87a478bee9f5e3f4b35965
shazamch <shazamrazzaq1234@gmail.com>
2024-12-16 16:54:06 +0500

    Cors Updated

--- Backend/server.js
+++ Backend/server.js
@@ -8,16 +8,19 @@ const http = require('http');
 const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
+
 const io = new Server(server, {
     cors: {
         origin: [
           "http://localhost:5173",
           "https://patient-communication-platform.vercel.app/",
+          "https://patient-communication-platform.vercel.app",
           ],
         methods: ['GET', 'POST'],
         credentials: true,
     },
 })
+
 app.use(express.json());
 app.use(cors);
 app.use(logger);
